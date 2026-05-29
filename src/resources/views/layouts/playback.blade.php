a4d2a25680fa67a5839a5f7af12f0bd72db9aa3e
IKEDA Youhei <youhey.ikeda@gmail.com>
2026-05-30 02:10:20 +0900

    Update playpipe favicon assets

--- src/resources/views/layouts/playback.blade.php
+++ src/resources/views/layouts/playback.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Episodes') - playpipe</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+    <link rel="icon" href="{{ asset('icon.svg') }}" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
     <style>
         :root {
             color-scheme: light;
